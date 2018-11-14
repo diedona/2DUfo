a2da085129d25279d7ecdd1a3725385a014ffab1
Diego Doná <diedona@gmail.com>
2018-11-14 00:23:32 -0200

    win condition

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,7 +5,7 @@ public class PlayerController : MonoBehaviour
 {
     public Rigidbody2D _rigidBody;
     public float _speed = 6.5f;
-    public Text bottomText;
+    public Text _bottomText;
 
     private Vector2 movement;
     private int points;
@@ -16,7 +16,7 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         _rigidBody = GetComponent<Rigidbody2D>();
-        bottomText = GameObject.Find("PointsText").GetComponent<Text>();
+        _bottomText = GameObject.Find("PointsText").GetComponent<Text>();
         this.gameOver = false;
     }
 
@@ -31,10 +31,6 @@ public class PlayerController : MonoBehaviour
         if (!this.gameOver)
         {
             _rigidBody.AddForce(movement, ForceMode2D.Impulse);
-        }
-        else
-        {
-            this.transform.position = Vector2.zero;
         }
     }
 
@@ -64,13 +60,12 @@ public class PlayerController : MonoBehaviour
 
     private void updatePointsText()
     {
-        bottomText.text = "PONTUAÇÃO: " + this.points.ToString();
+        _bottomText.text = "PONTUAÇÃO: " + this.points.ToString();
     }
 
     private void checkEndGame()
     {
         var pickups = GameObject.FindGameObjectsWithTag("PickUp");
-        print(pickups.Length);
         if (pickups.Length == 0)
         {
             this.endGame();
@@ -80,5 +75,8 @@ public class PlayerController : MonoBehaviour
     private void endGame()
     {
         this.gameOver = true;
+        _rigidBody.transform.position = Vector2.zero;
+        _rigidBody.velocity = Vector2.zero;
+        _bottomText.text = "GANHOU!";
     }
 }
