04e0541918656e6b48e2c9e5c49e79abdda4a662
Diego Doná <diedona@gmail.com>
2018-11-15 17:20:49 -0200

    second wave of pickups

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,8 @@ public class PlayerController : MonoBehaviour
     public Rigidbody2D _rigidBody;
     public float _speed = 6.5f;
     public Text _bottomText;
+    public GameObject _pickupsParent1;
+    public GameObject _pickupsParent2;
 
     private Vector2 movement;
     private int points;
@@ -66,17 +68,33 @@ public class PlayerController : MonoBehaviour
     private void checkEndGame()
     {
         var pickups = GameObject.FindGameObjectsWithTag("PickUp");
+
         if (pickups.Length == 0)
         {
-            this.endGame();
+            if (_pickupsParent1.activeInHierarchy)
+            {
+                _pickupsParent1.SetActive(false);
+                _pickupsParent2.SetActive(true);
+                this.resetPlayerPosition();
+            }
+            else
+            {
+                _pickupsParent2.SetActive(false);
+                this.endGame();
+            }
         }
     }
 
     private void endGame()
     {
         this.gameOver = true;
+        this.resetPlayerPosition();
+        _bottomText.text = "GANHOU! Total: " + this.points.ToString() + " pontos!";
+    }
+
+    private void resetPlayerPosition()
+    {
         _rigidBody.transform.position = Vector2.zero;
         _rigidBody.velocity = Vector2.zero;
-        _bottomText.text = "GANHOU!";
     }
 }
